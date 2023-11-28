a2b9cd016e594e0b8dd9fa253bca70b607a87150
Ana Fonseca <85508574+aff30@users.noreply.github.com>
2023-11-28 14:21:00 -0500

    contrast analysis

--- planned_contrast_analysis.R
+++ planned_contrast_analysis.R
@@ -1,3 +1,5 @@
+#Contrast Analysis 
+
 library(afex)
 library(tidyverse)
 library(dplyr)
@@ -5,7 +7,7 @@ library(psych)
 library(emmeans)
 library(graphics)
 
-## R contrast analysis 
+## R contrast analysis
 
 ##load data
 performance_data <- read.csv("2023_paper_performance_analysis.csv",
@@ -30,9 +32,9 @@ attributes(performance_data$TRT)
 
 #using the contrast in the lm model 
 #step 1: define the contrasts using the 7 rules 
-ProEo <- c(0,1,0.5,0)
-ProAbx <- c(0,1,0,-1)
-EoAbx <- c(0,0,0.5,-1)
+ContvsPro <- c(1,-1,0,0)
+ContvsEo <- c(1,0,-1,0)
+ProvsAbx <- c(0,1,0,-1)
 
 #CtlvsEoPrb <- c(-1, 0.5, 0.5, 0)
 #AtxvsEoPrb <- c(0,0.5,0.5, -1)
@@ -42,15 +44,15 @@ EoAbx <- c(0,0,0.5,-1)
 # Calculate the inverse of the contrast matrix
 #temp was added just to make the matrix square
 contr_temp <- rbind(temp=1,
-                    ProEo,
-                    ProAbx,
-                    EoAbx)
+                    ContvsPro,
+                    ContvsEo,
+                    ProvsAbx)
 
 
 #contr_temp <- rbind(temp=1,
-                   #CtlvsEoPrb,
-                  # AtxvsEoPrb,
-                   #ProbEo)
+#CtlvsEoPrb,
+# AtxvsEoPrb,
+#ProbEo)
 
 #Inverse of the the contr matrix using solve() function
 contr <- solve(contr_temp)
@@ -59,11 +61,11 @@ contr <- solve(contr_temp)
 contr.notemp <- contr[,-1]
 
 #step 3 - link the contrast to the predictors 
-contrasts(performance_data1$TRT) <- contr.notemp
-attributes(performance_data1$TRT)
+contrasts(performance_data$TRT) <- contr.notemp
+attributes(performance_data$TRT)
 
 #step 4 - run the linear model with planned contrasts
-m1 <- lm(LIVE_WEIGHT_STARTER ~ TRT, data = performance_data1)
+m1 <- lm(LIVE_WEIGHT_STARTER ~ TRT, data = performance_data)
 summary(m1)
 
 # Extract p-values
@@ -76,7 +78,7 @@ adjusted_p_m1 <- p.adjust(p_values1, method = "BH")
 print(adjusted_p_m1)
 round(adjusted_p_m1, 2)
 
-m2 <- lm(LIVE_WEIGHT_GROWER ~ TRT, data = performance_data1)
+m2 <- lm(LIVE_WEIGHT_GROWER ~ TRT, data = performance_data)
 summary(m2)
 
 # Extract p-values
@@ -89,7 +91,7 @@ adjusted_p_m2 <- p.adjust(p_values2, method = "BH")
 print(adjusted_p_m2)
 round(adjusted_p_m2, 2)
 
-m3 <- lm(LIVE_WEIGHT_TOTAL ~ TRT, data = performance_data1)
+m3 <- lm(LIVE_WEIGHT_TOTAL ~ TRT, data = performance_data)
 summary(m3)
 
 # Extract p-values
@@ -102,7 +104,7 @@ adjusted_p_m3 <- p.adjust(p_values3, method = "BH")
 print(adjusted_p_m3)
 round(adjusted_p_m3, 2)
 
-m4 <- lm(FI_STARTER.PEN ~ TRT, data = performance_data1)
+m4 <- lm(FI_STARTER.PEN ~ TRT, data = performance_data)
 summary(m4)
 
 # Extract p-values
@@ -115,7 +117,7 @@ adjusted_p_m4 <- p.adjust(p_values4, method = "BH")
 print(adjusted_p_m4)
 round(adjusted_p_m4, 2)
 
-m5 <- lm(FI_GROWER.PEN ~ TRT, data = performance_data1)
+m5 <- lm(FI_GROWER.PEN ~ TRT, data = performance_data)
 summary(m5)
 
 # Extract p-values
@@ -128,7 +130,7 @@ adjusted_p_m5 <- p.adjust(p_values5, method = "BH")
 print(adjusted_p_m5)
 round(adjusted_p_m5, 2)
 
-m6 <- lm(FI_TOTAL.PEN ~ TRT, data = performance_data1)
+m6 <- lm(FI_TOTAL.PEN ~ TRT, data = performance_data)
 summary(m6)
 
 # Extract p-values
@@ -141,7 +143,7 @@ adjusted_p_m6 <- p.adjust(p_values6, method = "BH")
 print(adjusted_p_m6)
 round(adjusted_p_m6, 2)
 
-m7 <- lm(FCR_STARTER.PEN ~ TRT, data = performance_data1)
+m7 <- lm(FCR_STARTER.PEN ~ TRT, data = performance_data)
 summary(m7)
 
 # Extract p-values
@@ -154,7 +156,7 @@ adjusted_p_m7 <- p.adjust(p_values7, method = "BH")
 print(adjusted_p_m7)
 round(adjusted_p_m7, 2)
 
-m8 <- lm(FCR_GROWER.PEN ~ TRT, data = performance_data1)
+m8 <- lm(FCR_GROWER.PEN ~ TRT, data = performance_data)
 summary(m8)
 
 # Extract p-values
@@ -211,3 +213,54 @@ Atx <- performance_data1 %>%
 descriptive_atx <- describe(Atx[,2:13], IQR = TRUE)
 
 citation(package = "emmeans")
+
+library(gtExtras)
+library(svglite)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(ggExtra)
+
+##Descriptive graph 
+performance_data <- na.omit(performance_data) 
+performance_data = subset(performance_data, 
+                          select = -c(ADG_STARTER.PEN,FI_STARTER.PEN,ADG_GROWER.PEN,FI_GROWER.PEN,ADG_TOTAL.PEN,FI_TOTAL.PEN,EO,PROB,ABX) )
+discriptive_graph <- gt_plt_summary(performance_data)
+
+ggsave("discriptive_graph_BD.pdf")
+
+Probiotic <- performance_data %>% 
+  filter(TRT == "Probiotic")
+
+PBdescrip <- subset(PB, 
+                    select = -c(ADG_STARTER.PEN,FI_STARTER.PEN,ADG_GROWER.PEN,FI_GROWER.PEN,ADG_TOTAL.PEN,FI_TOTAL.PEN,EO,PROB,ABX) )
+
+discriptive_graph_PB <- gt_plt_summary(Probiotic)
+
+
+EssentialOils <- performance_data %>% 
+  filter(TRT == "Essential Oils")
+
+discriptive_graph_EO <- gt_plt_summary(EssentialOils)
+
+Antibiotic <- performance_data %>% 
+  filter(TRT == "Antibiotic")
+
+discriptive_graph_ATB <- gt_plt_summary(Antibiotic)
+
+BasalDiet <- performance_data %>% 
+  filter(TRT == "Basal Diet")
+
+discriptive_graph_BD <- gt_plt_summary(BasalDiet)
+
+
+# Save the scatter plot in a variable
+p <- ggplot(performance_data, aes(x = TRT, y = LIVE_WEIGHT_STARTER, color = TRT)) +
+  geom_point() + theme_bw()
+
+# Marginal histograms by group
+ggMarginal(p, size = 2, type = "histogram", 
+           groupColour = TRUE,
+           groupFill = TRUE)
+
+
